d58a02f1c1f9e16c15b1d696f81af374d40aae7d
Shreedhar <72458005+netsfreak@users.noreply.github.com>
2026-06-27 02:24:30 +0530

    Add: sql-alter-table - submission-1

---
+++ SQL/sql-alter-table/submission-1.sql
@@ -0,0 +1,16 @@
+CREATE TABLE books (
+  id INTEGER,
+  title TEXT,
+  author TEXT
+);
+-- Do not modify above this line --
+
+ALter table books ADD COLUMN published_year INTEGER;
+Alter Table books RENAME COlumn id to isbn;
+alter table books DROP Column author;
+
+-- Do not modify below this line --
+SELECT column_name, data_type, column_default
+FROM information_schema.columns
+WHERE table_name = 'books'
+ORDER BY column_name;
